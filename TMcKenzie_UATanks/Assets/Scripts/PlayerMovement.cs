1bfdbb7a45ef035f388bf4100743ef28fb7df0ec
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-06-04 02:49:32 -0700

    Making Movement Scripts
    Forward and backwards are good, turning, not so much.

---
+++ TMcKenzie_UATanks/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform playerTF;
+    public Rigidbody playerRB;
+    public float playerForwardSpeed = 0.09f;
+    public float playerBackSpeed = 0.03f;
+    public float playerTurnSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTF = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerTF.position += Vector3.forward * playerForwardSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerTF.position += Vector3.back * playerBackSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion wantedRotation = playerTF.rotation * Quaternion.Euler(Vector3.up * playerTurnSpeed);
+            playerRB.MoveRotation(wantedRotation);
+        }
+    }
+}
